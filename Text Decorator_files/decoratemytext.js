d64c95ecc85eb820939c31bc4814ec8fe0c5b0c5
Ochirsuren <ochiroo13@gmail.com>
2020-03-06 14:48:43 -0600

    Daily push text decoration not finished

---
+++ Text Decorator_files/decoratemytext.js
@@ -0,0 +1,9 @@
+function biggerDecoration() {
+	document.getElementById("text").style.fontSize = "24px";
+}
+
+function blingChanged() {
+	var checkd = $("#bling").attr("checked");
+	console.log(checked);
+	document.getElementById("text").style.fontWeight = "bold";
+}
