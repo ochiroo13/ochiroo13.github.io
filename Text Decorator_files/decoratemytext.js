e7083c4215f4b4f55d6cedc047dc2634c2f62b57
Ochirsuren <ochiroo13@gmail.com>
2020-03-07 10:36:25 -0600

    Homework: Text Decorator, Javascript questions

--- Text Decorator_files/decoratemytext.js
+++ Text Decorator_files/decoratemytext.js
@@ -1,9 +1,72 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+var isTimerStarted = false;
+var timer;
+
 function biggerDecoration() {
-	document.getElementById("text").style.fontSize = "24px";
+
+	if (isTimerStarted) {
+		isTimerStarted = false;
+
+		clearInterval(timer);
+	}
+	else {
+		isTimerStarted = true;
+
+		timer = setInterval(() => {
+			biggerDec();
+		}, 2000);
+	}
 }
 
 function blingChanged() {
-	var checkd = $("#bling").attr("checked");
-	console.log(checked);
-	document.getElementById("text").style.fontWeight = "bold";
+	let checked = document.getElementById("bling").checked;
+
+	if (checked) {
+
+	document.getElementById("text").style.fontWeight = "700";
+	document.getElementById("text").style.textDecoration = "underline";
+	document.getElementById("text").style.color = "green";
+	document.body.style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+	console.log("true");
+	}
+	else {
+
+	document.getElementById("text").style.fontWeight = "400";
+	document.getElementById("text").style.textDecoration = "none";
+	document.getElementById("text").style.color = "black";
+	document.body.style.backgroundImage = "none";
+	console.log("false");
+	}
+
+}
+
+function biggerDec() {
+	let textElem = document.getElementById("text");
+
+	if (textElem.style.fontSize.length == 0) {
+		document.getElementById("text").style.fontSize = "16px";
+	}
+	textElem.style.fontSize = (parseInt(textElem.style.fontSize.slice(0, 2)) + 2) + "px";
+}
+
+function doIgpayAtinlay() {
+
+}
+
+function doMalkovich() {
+	let textElem = document.getElementById("text");
+	let words = textElem.value.split(" ");
+	let res = "";
+	for (let i = 0; i < words.length; i ++) {
+		if (words[i].length >= 5)
+			words[i]  = "Malkovich";
+
+		res += words[i] + " ";
+	}
+
+	document.getElementById("text").value = res;
 }
